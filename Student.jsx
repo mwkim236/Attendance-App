a8c4b55b315e1826e93491a73c815eb34ecd6fce
Min Kim <mwkim236@gmail.com>
2022-08-12 00:29:00 -0400

    finished attendance app

---
+++ Student.jsx
@@ -0,0 +1,23 @@
+const Student = ({ name, dispatch, id, isHere }) => {
+    return (
+        <div>
+            <span 
+                style={{
+                    textDecoration: isHere ? "line-through" : 'none',
+                    color: isHere ? 'gray' : 'black'
+                }}
+                onClick={() => {
+                    dispatch({type: "mark-student", payload: { id } })
+                }}>
+                {name}
+            </span>
+            
+            <button onClick={() => {
+                dispatch({
+                    type: 'delete-student',
+                    payload: { id }
+                })
+            }}>Delete</button>
+        </div>
+    )
+}
--- attendance-app.jsx
+++ attendance-app.jsx
@@ -1,32 +1,79 @@
-const reducer = (state, action) => {}
-
 const initialState = {
     count: 0,
-    students: [
-        {
-            id: Date.now(),
-            name: 'John',
-            isHere: false
-        }
-    ]
+    students: [],
 };
+ 
+
+const reducer = (state, action) => {
+    switch(action.type) {
+        case 'add-student':
+            const name = action.payload.name;
+            const newStudent = {
+                id: Date.now(),
+                name,
+                isHere: false,
+            };
+            return {
+                count: state.count + 1,
+                students: [...state.students, newStudent],
+            };
+        
+        case 'delete-student':
+            return {
+                count: state.count - 1,
+                students: state.students.filter((student) => {
+                    student.id !== action.payload.id
+                }),
+            }
+
+        case 'mark-student':
+            return {
+                count: state.count,
+                students: state.students.map(student => {
+                    if (student.id === action.payload.id) {
+                        return { ...student, isHere: !student.isHere};
+                    }
+                    return student;
+                }),
+            }
+        
+        default: 
+            return state;
+    }
+}
+
 
 function App() {
     const { useState, useEffect, useReducer } = React;
     const [name, setName] = useState('');
     const [studentsInfo, dispatch] = useReducer(reducer, initialState);
 
+    console.log('rerendering...')
     return (
         <div>
             <h1>Attendance</h1>
             <p>Total Number of Students: {studentsInfo.count}</p>
+        
             <input 
                 type="text"
                 placeholder="Enter student name"
                 value={name}
                 onChange={e => setName(e.target.value)} 
             />
-            <button>Add</button>
+            <button onClick={() => {
+                dispatch({type: 'add-student', payload: {name}})
+            }}>Add</button>
+
+            {studentsInfo.students.map(student => {
+                return (
+                    <Student 
+                        key={student.id} 
+                        name={student.name} 
+                        dispatch={dispatch}
+                        id={student.id}
+                        isHere={student.isHere}
+                    />);
+            })}
         </div>
     )
 }
